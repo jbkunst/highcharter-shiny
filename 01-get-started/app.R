3b1ca1d43172dabca69fc7b70349e35fa4144e9f
Joshua Kunst <jbkunst@gmail.com>
2026-07-14 13:42:32 -0400

    Modernize getting started app theme

--- 01-get-started/app.R
+++ 01-get-started/app.R
@@ -1,32 +1,30 @@
-# Load packages
 library(shiny)
-library(shinythemes)
+library(bslib)
 library(highcharter)
 
+app_theme <- bs_theme(
+  version = 5,
+  bootswatch = "flatly",
+  primary = "#47475c",
+  success = "#a3edba"
+)
+
 ui <- fluidPage(
-  theme = shinytheme("paper"),
+  theme = app_theme,
   fluidRow(
     column(width = 6, highchartOutput("chart1")),
-    column(width = 6, highchartOutput("chart2")),
+    column(width = 6, highchartOutput("chart2"))
   )
-               
 )
 
 server <- function(input, output) {
-  
   output$chart1 <- renderHighchart({
-    
     highcharts_demo()
-    
   })
-  
+
   output$chart2 <- renderHighchart({
-    
     hchart(iris, "scatter", hcaes(Sepal.Length, Sepal.Width, group = Species))
-    
   })
-  
-  
 }
 
 shinyApp(ui, server)
